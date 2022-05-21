88c3165af0b9903ae7408253236a9b9685cdf165
Iyadh <ibenouarred@gmail.com>
2022-05-21 14:11:29 +0100

    Initial commit

---
+++ app/src/main/java/tn/edu/esprit/ba9chich/MainActivity.kt
@@ -0,0 +1,11 @@
+package tn.edu.esprit.ba9chich
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
